93d47a59f5e67bea9927f46d501d970322eeffa8
Adrian <dbarra@agetic.gob.bo>
2019-10-29 15:00:01 -0400

    moficando el schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,27 +1,72 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLBoolean, GraphQLSchema } = graphql;
 
 const courses = [
   {
     id: '1',
     name: 'Como usar Babel',
     language: 'Javascript',
-    date: '2019'
+    date: '2019',
+    professorId: 2
   },
   {
     id: '2',
     name: 'Programacion orientada a Objetos',
     language: 'Java',
-    date: '2020'
+    date: '2020',
+    professorId: 1
   },
   {
     id: '3',
-    name: 'Algoritmos geneticps',
+    name: 'Algoritmos Geneticos',
     language: 'Java',
-    date: '2021'
+    date: '2021',
+    professorId: 1
+  }
+];
+
+const professors = [
+  {
+    id: 1,
+    name: 'Adrian',
+    age: 27,
+    active: true,
+    date: '2022'
+  },
+  {
+    id: 2,
+    name: 'Marcelo',
+    age: 30,
+    active: true,
+    date: '2022'
+  },
+  {
+    id: 3,
+    name: 'Ivan',
+    age: 24,
+    active: false,
+    date: '2022'
+  }
+];
+
+const users = [
+  {
+    id: 1,
+    name: 'Adrian',
+    email: 'adry.dabp@gmail.com',
+    password: '123',
+    date: '2022'
+  },
+  {
+    id: 2,
+    name: 'Ivan',
+    email: 'ivan@gmail.com',
+    password: '789465',
+    date: '2022'
   }
 ]
+
 
 const CourseType = new GraphQLObjectType({
   name: 'Course',
@@ -29,6 +74,34 @@ const CourseType = new GraphQLObjectType({
     id: {type: GraphQLString},
     name: {type: GraphQLString},
     language: {type: GraphQLString},
+    date: {type: GraphQLString},
+    professor: {
+      type: ProfessorType,
+      resolve(parent, args) {
+        return professors.find(professor => professor.id === parent.professorId)       
+      }
+    }
+  })
+});
+
+const ProfessorType =  new GraphQLObjectType({
+  name: 'Professor',
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    age: {type: GraphQLInt},
+    active: {type: GraphQLBoolean},
+    date: {type: GraphQLString}
+  })
+});
+
+const UserType =  new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {type: GraphQLID},
+    name: {type: GraphQLString},
+    email: {type: GraphQLString},
+    password: {type: GraphQLString},
     date: {type: GraphQLString}
   })
 });
@@ -40,11 +113,33 @@ const RootQuery = new GraphQLObjectType({
       type: CourseType,
       args: {
         id: {
-          type: GraphQLString
+          type: GraphQLID
         }
       },
       resolve(parent, args) {
         return courses.find(item => item.id === args.id);
+      }
+    },
+    professor: {
+      type: ProfessorType,
+      args: {
+        name: {
+          type: GraphQLString
+        }
+      },
+      resolve(parent, args) {
+        return professors.find(professor => professor.name === args.name);
+      }
+    },
+    user: {
+      type: UserType,
+      args: {
+        email: {
+          type: GraphQLString
+        }
+      },
+      resolve(parent, args) {
+        return users.find(user => user.email === args.email);
       }
     }
   }
